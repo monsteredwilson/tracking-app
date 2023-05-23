706f04bd760d210c329c3ba02fe8a6f1a3c014c6
Adryel César <adryelcesarm@gmail.com>
2023-05-23 17:43:54 -0300

    atualizacao de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { FormRegister } from './components/FormRegister'
 import BackGroundImage from "./assets/bg.jpg"
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route, Router } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Home } from './pages/Home'
 import { Dashboard } from './pages/Dashboard'
@@ -17,14 +17,16 @@ function App() {
 
 	return (
 		<div className="App">
-			<Routes>
-				<Route path='/' element={<Home/>}/>
-				<Route path='/login' element={<Login/>}/>
-				<Route path='/dashboard' element={<Dashboard/>}/>
-				<Route path='/adminpanel' element={<AdminPanel/>}/>
-			</Routes>
-			<GlobalStyle/>
-			<ToastContainer/>
+
+			{/* <Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/dashboard' element={<Dashboard />} />
+				<Route path='/adminpanel' element={<AdminPanel />} />
+			</Routes> */}
+
+			<GlobalStyle />
+			<ToastContainer />
 		</div>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext'
 import App from './App'
 import './index.css'
 import { AdminProvider } from './contexts/AdminContext'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Dashboard } from './pages/Dashboard'
+import { AdminPanel } from './pages/Admin'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
@@ -14,6 +18,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 				{/* <StateFilterProvider> */}
 				<App />
 				{/* </StateFilterProvider> */}
+
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/adminpanel' element={<AdminPanel />} />
+				</Routes>
 			</UserProvider>
 		</AdminProvider>
 
