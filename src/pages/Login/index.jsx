2b8a3988ab0df1b7027a9b1530d7814d83c97643
Adryel César <adryelcesarm@gmail.com>
2023-05-23 18:45:26 -0300

    teste new route

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,15 @@
 import { PageLoginContainer } from "./styled"
 import { FormLogin } from "../../components/FormLogin"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom/dist"
 
 export function Login(){
+	const navigate = useNavigate()
+
+	useEffect(()=>{
+
+	},navigate('/login'))
+
 	return(
 		<PageLoginContainer>
 			<FormLogin/>
