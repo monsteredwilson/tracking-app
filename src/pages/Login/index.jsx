83732def9b750563943ff3eaacc8ffc1f97e1f62
Adryel César <adryelcesarm@gmail.com>
2023-05-23 18:06:52 -0300

    att build

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,15 @@
 import { PageLoginContainer } from "./styled"
 import { FormLogin } from "../../components/FormLogin"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 export function Login(){
+	navigate = useNavigate()
+
+	useEffect(()=>{
+
+	},[])
+
 	return(
 		<PageLoginContainer>
 			<FormLogin/>
